4c46938b1132df8daa94a886ce98c3df87bb0133
Stancw <yolo_stance@protonmail.com>
2019-05-05 22:06:00 +0200

    funny version

---
+++ start.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+
+db = SQLAlchemy(app)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(1024), nullable=False)
+
+    def __init__(self, text, tags):
+        self.text = text.strip()
+        self.tags = [
+            Tag(text=tag.strip()) for tag in tags.split(',')
+        ]
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(32), nullable=False)
+
+    message_id = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=False)
+    message = db.relationship('Message', backref=db.backref('tags', lazy=True))
+
+
+db.create_all()
+
+
+
+@app.route('/main', methods=['GET'])
+def main():
+    return render_template('main.html')
+
+
+@app.route('/posts', methods=['GET'])
+def posts():
+    return render_template('posts.html', messages=Message.query.all())
+
+
+@app.route('/add_message', methods=['POST'])
+def add_message():
+    text = request.form['text']
+    tag = request.form['tag']
+
+    db.session.add(Message(text, tag))
+    db.session.commit()
+
+    return redirect(url_for('posts'))
+
+
+@app.route('/sites')
+def sites():
+    return render_template('sites.html')
+
+
+@app.route('/about_me')
+def about_me():
+    return render_template('about_me.html')
+
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
